328aa9496d21cb06619f5136e8483b017a75d6fc
RamunasS <ramunas.songaila@gmail.com>
2020-08-22 11:38:22 +0300

    added overloaded constructors sample

---
+++ src/main/java/advancedfeatures/lesson3/overload/Constructors.java
@@ -0,0 +1,32 @@
+package advancedfeatures.lesson3.overload;
+
+import com.sun.org.apache.xerces.internal.impl.xs.util.XInt;
+import com.sun.org.apache.xerces.internal.impl.xs.util.XIntPool;
+
+import java.util.concurrent.LinkedTransferQueue;
+
+public class Constructors {
+
+    private int num = 0;
+    private String text = "";
+
+    public Constructors() {
+    }
+
+    public Constructors(int num) {
+        this();     //chained constructor - calls Constructor()
+        this.num = num;
+    }
+
+    public Constructors(int num, String text) {
+        this(num);     // chained constructor, paprastai savyje kviecia konstruktoriu, turinti maziau parametru
+        this.text = text;
+
+    }
+
+    public Constructors(int num2, String othertext, int num3) {
+        this(num2, othertext);
+        this.num += num3;
+    }
+
+}
